170f6fa3695ef3b05e1838b166e6260bb5dbbce9
Tom Bernard <bernard5150@gmail.com>
2017-01-24 15:55:35 -0800

    jj

--- src/database.js
+++ src/database.js
@@ -2,10 +2,11 @@
 
 var mongoose = require('mongoose');
 
+mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://localhost/quizz-app', function(err) {
 
   if (err) {
-    console.log("Failed connecting to Mongodb!");
+    console.log("Failed connecting to Mongodb!" + err);
   } else {
     console.log("Successfully connected to Mongodb!");
   }
