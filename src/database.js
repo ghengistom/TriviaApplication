2885e9b26ff81d31d323d446d2bd4cb2993a3527
Tom Bernard <bernard5150@gmail.com>
2017-01-24 14:21:06 -0800

    Wired up mongodb to aws

--- src/database.js
+++ src/database.js
@@ -2,7 +2,7 @@
 
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/quizz-app', function(err) {
+mongoose.connect('mongodb://tom/kikiscratchedme@ec2-52-52-136-108.us-west-1.compute.amazonaws.com:27017/questions', function(err) {
 
   if (err) {
     console.log("Failed connecting to Mongodb!");
