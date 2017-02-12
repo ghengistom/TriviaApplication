ca9ed63d8cf4a2631c9738061f040525c438701e
Tom Bernard <bernard5150@gmail.com>
2017-02-11 17:04:24 -0800

    more styling to signin

--- public/signin.js
+++ public/signin.js
@@ -1,54 +1,5 @@
-
-var email;
-
-$("#signbtn1").click(function(){
-    email = $("#psw").val();
-    console.log(email);
-    //alert("Value: " + $("#psw").val());
-    var object = {"email": email}
-
-
-    var sobject = JSON.stringify(object);
-    console.log("this is the contents of the email variable not stringified" + object);
-    console.log("this is the contents of the email variable" + sobject);
+document.querySelector(".button1").addEventListener("click", function(){
+    var element = document.createElement("div");
+    element.innerHTML = "<p>Additional Line</p><p>Additional Line</p><p>Additional Line</p>";
+  document.querySelector(".content").appendChild(element);
 });
-
-
-/*
-    // Attach a submit handler to the form
-    $( "#searchForm" ).submit(function( event ) {
-
-      // Stop form from submitting normally
-      event.preventDefault();
-
-      // Get some values from elements on the page:
-      var $form = $( this ),
-        term = $form.find( "input[name='s']" ).val(),
-        url = $form.attr( "action" );
-
-      // Send the data using post
-      var posting = $.post( /signin, { s: term } );
-
-      // Put the results in a div
-      posting.done(function( data ) {
-        var content = $( data ).find( "#content" );
-        $( "#result" ).empty().append( content );
-      });
-    });
-
-*/
-
-
-
-
-/*
-
-var emailpost = $.ajax({
-  type: "POST",
-  url: url,
-  data: data,
-  success: success,
-  dataType: dataType
-});
-});
-*/
